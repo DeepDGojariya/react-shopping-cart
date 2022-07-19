ddf097fbad98c8d7b7d403f77da7f7d2e02c2efc
Deep <deepgojariya123@gmail.com>
2022-07-19 12:49:53 +0530

    Product Component Created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
+import React, { Component } from "react";
+import './index.css'
+import data from'./data.json'
+import Products from "./components/Products";
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      products:data.products,
+      size:""
+    }
+  }
 
 
-function App() {
-  return (
-    <>
-    <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Navbar w/ text</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-      <span className="navbar-text">
-        Admin
-      </span>
-  </div>
-</nav>
-<footer className="bg-light text-center" style={{position:"fixed",bottom:0,width:"100%",padding:"10px 10px 0px 10px",height:"40px"}}>All Rights Reserved</footer>
-</>
-  );
+  render() {
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg bg-dark navbar-dark navbar-fixed-top">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">Navbar w/ text</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <span className="navbar-text">
+              Admin
+            </span>
+          </div>
+        </nav>
+        <div className="content">
+          <div className="main"><Products products={this.state.products}/></div>
+          <div className="sidebar">Cart-Items</div>
+        </div>
+        <footer className="bg-dark text-light text-center " style={{ position: "fixed", bottom: 0, width: "100%", padding: "10px 10px 0px 10px", height: "40px"}}>All Rights Reserved</footer>
+      </>
+    );
+  }
 }
 
 export default App;
+
