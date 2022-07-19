14632a857f172f413654a506027aa87e15de01dc
Deep <deepgojariya123@gmail.com>
2022-07-19 14:52:33 +0530

    Filter&Sort Completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React, { Component } from "react";
 import './index.css'
-import data from'./data.json'
+import data from './data.json'
 import Products from "./components/Products";
+import Filter from "./components/Filter";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super()
-    this.state={
-      products:data.products,
-      size:""
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: ""
+    }
+  }
+
+  sortProducts = (event) => {
+
+    const sort = event.target.value
+    console.log(sort)
+    this.setState({
+      sort: sort,
+      products: this.state.products.slice().sort((a, b) =>
+        sort === "lowest" ? a.price > b.price ? 1 : -1 : sort === "highest" ? a.price < b.price ? 1 : -1 : a._id < b._id ? 1 : -1)
+    })
+  }
+
+  filterProducts = (event) => {
+    if (event.target.value === "") {
+      this.setState({
+        products: data.products,
+        size: event.target.value
+      })
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(product => product.availableSizes.indexOf(event.target.value) >= 0)
+      })
     }
   }
 
@@ -28,10 +55,17 @@ class App extends Component {
           </div>
         </nav>
         <div className="content">
-          <div className="main"><Products products={this.state.products}/></div>
+          <div className="main">
+            <Filter count={this.state.products.length}
+              size={this.state.size}
+              sort={this.state.sort}
+              filterProducts={this.filterProducts}
+              sortProducts={this.sortProducts}></Filter>
+            <Products products={this.state.products} />
+          </div>
           <div className="sidebar">Cart-Items</div>
         </div>
-        <footer className="bg-dark text-light text-center " style={{ position: "fixed", bottom: 0, width: "100%", padding: "10px 10px 0px 10px", height: "40px"}}>All Rights Reserved</footer>
+        <footer className="bg-dark text-light text-center " style={{ position: "fixed", bottom: 0, width: "100%", padding: "10px 10px 0px 10px", height: "40px" }}>All Rights Reserved</footer>
       </>
     );
   }
